42177e8926ef49fdca410e366e8dde8b2942b36f
didskfo <didskfo2003@gmail.com>
2025-03-26 19:06:29 +0900

    feat(setting): spring boot init

---
+++ src/main/java/com/github/kmu_wink/yeogichadae2/Yeogichadae2Application.java
@@ -0,0 +1,13 @@
+package com.github.kmu_wink.yeogichadae2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yeogichadae2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Yeogichadae2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/kmu_wink/yeogichadae2/Yeogichadae2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kmu_wink.yeogichadae2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Yeogichadae2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
